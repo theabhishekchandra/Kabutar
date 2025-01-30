b42465c07ca27bd373d69898ee98eb0061ad68b8
Abhishek Chandra <ac927920@gmail.com>
2025-01-30 23:38:19 +0530

    feat: add app shared preference and core constants
    Add `APPSharedPref` and `IAPPSharedPref` for managing shared preferences.
    Add `MainConst` for core constants.
    Add `CoreApplication` to the application in `AndroidManifest.xml`.
    Update `MainActivity` to `layout.MainActivity` in `AndroidManifest.xml`.

---
+++ app/src/main/java/com/abhishek/gomailai/core/MainConst.kt
@@ -0,0 +1,12 @@
+package com.abhishek.gomailai.core
+
+object MainConst {
+    const val SHARED_PREFERENCE_DB: String = "go_mail_ai_shared_pref"
+    const val IS_USER_LOGGED: String = "is_user_logged"
+    const val USER_NAME: String = "user_name"
+    const val USER_MOBILE_NUMBER: String = "user_mobile_number"
+    const val USER_EMAIL: String = "user_email"
+    const val USER_PASSWORD: String = "user_password"
+    const val AI_ACCESS_KEY: String = "ai_access_key"
+    const val AI_MODEL: String = "ai_model"
+}
