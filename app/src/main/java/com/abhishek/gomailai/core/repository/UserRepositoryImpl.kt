551ebf81d350e6e027220f7c87f0a2cde78b51e1
Abhishek Chandra <ac927920@gmail.com>
2025-02-02 02:30:58 +0530

    [Task 1] feat: add UserRepository, UsersEntity, and related database components
    Add `UserRepositoryImpl` and `IUserRepository` to manage user data.
    Add `UsersEntity` for user data storage.
    Add `UsersDao` to provide access to the users table.
    Add `DBResponseModel` for database operation results.
    Add `AppDatabase` for Room database setup.
    Add `DatabaseModule` to provide database-related dependencies.
    Add `DatabaseConst` for database constants.
    Add `UserViewModel` for user-related operations.
    Rename `MainConst` to `MainConst` and move it to `utils` package.
    Add `insertUser`, `getAllUsers`, and `deleteUser` methods to `UserRepositoryImpl`.
    Add `insertUser`, `updateUser`, `deleteUser`, and `getAllUsers` methods to `UsersDao`.
    Update to use indexed columns for name and email in `UsersEntity`.

---
+++ app/src/main/java/com/abhishek/gomailai/core/repository/UserRepositoryImpl.kt
@@ -0,0 +1,46 @@
+package com.abhishek.gomailai.core.repository
+
+import com.abhishek.gomailai.core.local.DBResponseModel
+import com.abhishek.gomailai.core.local.dao.UsersDao
+import com.abhishek.gomailai.core.local.entities.UsersEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val usersDao: UsersDao
+) : IUserRepository {
+
+    override suspend fun insertUser(user: UsersEntity): DBResponseModel<Unit> {
+        try {
+            usersDao.insertUser(user)
+            return DBResponseModel.Success(Unit)
+        } catch (e: Exception) {
+            return DBResponseModel.Error(e.message ?: "Unknown error occurred")
+
+        }
+    }
+
+    override fun getAllUsers(): Flow<DBResponseModel<List<UsersEntity>>> = flow {
+        try {
+            usersDao.getAllUsers().collect { users ->
+                emit(DBResponseModel.Success(users))
+            }
+        } catch (e: Exception) {
+            emit(DBResponseModel.Error(e.localizedMessage ?: "Unknown Error"))
+        }
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun deleteUser(user: UsersEntity): DBResponseModel<Unit> {
+        return try {
+            usersDao.deleteUser(user)
+            DBResponseModel.Success(Unit)
+        } catch (e: Exception) {
+            DBResponseModel.Error("Delete failed: ${e.message}", e)
+        }
+    }
+}
