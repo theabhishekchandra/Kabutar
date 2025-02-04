9ff8c32c1ad4b05a2d4ac0d200a811366fccd76d
Abhishek Chandra <ac927920@gmail.com>
2025-02-04 12:55:55 +0530

    [Task 2] feat: integrate Secrets Gradle plugin and update API key handling
    Integrate the Secrets Gradle plugin for managing API keys in a `secrets.properties` file.
    Update the `build.gradle.kts` files to include and apply the Secrets Gradle plugin.
    Move API key values from `local.properties` to `secrets.properties`.
    Add `secrets.properties` to `.gitignore`.
    Update `AndroidManifest.xml` to use the values from the plugin.
    Update color of `button_state_animator.xml`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.secrets.gradle.plugin) apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("androidx.room") version "2.6.1" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
