37e32e0b7ba5f294b4f5e2ba0ff068e8f1573c15
Abhishek Chandra <ac927920@gmail.com>
2026-09-01 03:01:16 +0530

    Define mail template data model

--- app/src/main/java/com/abhishek/gomailai/core/repository/template/MailTemplateDM.kt
+++ app/src/main/java/com/abhishek/gomailai/core/repository/template/MailTemplateDM.kt
@@ -1,5 +1,9 @@
 package com.abhishek.gomailai.core.repository.template
 
 data class MailTemplateDM(
-
+    val id: String = "",
+    val title: String = "",
+    val industry: String = "",
+    val body: String = "",
+    val tags: List<String> = emptyList(),
 )
